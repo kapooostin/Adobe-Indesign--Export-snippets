a137399751a030fd8d7eb7f50b0477966fce85e7
Vladimir Kapustin <kapooostin@gmail.com>
2011-07-16 16:49:10 +0400

    IMPROVE
    Disabled grouping.
    Now export dialog is called, when there are more than one object in a snippet.
    
    Added each() for iterating;

--- export_snippets.jsx
+++ export_snippets.jsx
@@ -1,16 +1,19 @@
-﻿( function main () {
+﻿//[TODO] обрабатывать ошибку для файлов без путей -- не сохраненных.
+
+( function main () {
 	// var start_time = new Date(), timing;
 	// writeln( "/////////////" );
 	if ( app.documents.length > 0 && app.activeDocument ) {
 		var doc = app.activeDocument,
 			spreads = doc.spreads,
 			snippet_regex = /^.*\.idms/,
+			export_command = app.menuActions.itemByName( "Export..." ),
 			exclude_filename_frame = function ( item, array ) {
-				for ( var i = 0, max = array.length; i < max; i++ ) {
-					if ( array[ i ] === item ) {
+				each( array, function delete_filename_frame ( array_item, i, array ) {
+					if ( array_item === item ) {
 						array.splice( i, 1 );
 					}
-				}
+				} );
 			},
 			get_snippet_export_params = function ( texts ) {
 				var max = texts.length,
@@ -61,11 +64,18 @@
 						var snippet_controller = new Controller(),
 							target;
 						if ( items.length > 1 ) {
-							target = spread.groups.add(items);
+							//target = spread.groups.add(items);
 							snippet_controller.export = function( /*File*/ file ){
-								var result = this._export( target, file );
-								target.ungroup();
-								return result;
+								//var result = this._export( target, file );
+								//target.ungroup();
+								each( items, 
+									function select_items ( item ) {	
+										item.select( SelectionOptions.ADD_TO );
+									}
+								);
+								export_command.invoke();
+								doc.select( [], SelectionOptions.REPLACE_WITH );
+								return true;
 							};
 						} else {
 							target = items[ 0 ];
@@ -90,15 +100,33 @@
 			},
 			writeln = function () {
 				$.writeln( sprintf.apply ( this, arguments ) );
+			},
+			each = function(obj, iterator, context) {
+				var value;
+				if (obj == null) return;
+				if ( obj.length && obj.length > 0 ) {
+					for (var i = 0, l = obj.length; i < l; i++) {
+						if (iterator.call(context, obj[i], i, obj) === {}) return;
+					}
+				} else {
+					for (var key in obj) {
+						if (hasOwnProperty.call(obj, key)) {
+							if (iterator.call(context, obj[key], key, obj) === {}) return;
+						}
+					}
+				}
 			};
 		
 		return report_results ( 
 			( function export_all_snippets ( spreads ) {
 				var is_successful = true;
-
-				for ( var i = 0, max = spreads.length; i < max; i++ ) {
-					is_successful = is_successful && export_snippet( spreads[ i ] );
-				}
+				
+				each( spreads, 
+					function export_snippet_and_report ( item ) {
+						item.select();
+						is_successful = is_successful && export_snippet( item );
+					} 
+				);
 				// timing = (new Date()) - start_time;
 				// writeln( "///////%s//////", timing );
 				return is_successful;
